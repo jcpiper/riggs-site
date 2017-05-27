932e6b782f196b6f8df175ce958e66d9eb7e6f9c
Justin Piper <jpiper4198@gmail.com>
2017-05-27 12:21:53 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="description" content="Contact and appointment info for West Main Optometry"/>
+    <meta name="keywords" content="Kim, Riggs, Pelger, Optometry, Eye, Doctor, Glasses, Spectacles, Appointment, Appointments,
+        Eyes, Optometrist, Contacts, Contact, Lens, Lenses"/>
+    <meta name="author" content="Justin Piper"/>
+    <title>West Main Optometry</title>
+    <link rel="stylesheet" type="text/css" href="public/stylesheets/main.css"/>
+  </head>
+  
+  <body>
+    <header>
+      <!-- nav bar -->
+      <nav id="navBar">
+        Navigation bar will go here :)
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            About
+          </li>
+          <li>
+            Appointment
+          </li>
+          <li>
+            Contact
+          </li>
+        </ul>
+      </nav>
+    </header>
+      <!-- #Content-->
+    <div>
+      <h2>Welcome!</h2>
+      <p>
+        Info will go here
+      </p>
+      <h2>About Us</h2>
+      <p>
+        Some more will go here
+      </p>
+      <h2>Make An Appointment</h2>
+      <p>
+        And so on
+      </p>
+      <h2>Contact Us</h2>
+      <p>
+        etc.
+      </p>
+    </div>
+    <footer>
+      <!-- footer stuff -->
+      Footer stuff ;)
+    </footer>
+  </body>
+</html>
+
+
+
