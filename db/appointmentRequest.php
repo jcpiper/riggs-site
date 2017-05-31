0d51be77f579349de98af569cc9e44a2d9fdea55
Justin Piper <jpiper4198@gmail.com>
2017-05-31 12:44:38 -0400

    Background images + initial db stuff

---
+++ db/appointmentRequest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Justin
+ * Date: 5/31/2017
+ * Time: 12:42 PM
+ */
+
+// Handles appointment requests
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Justin Piper"/>
     <title>West Main Optometry</title>
     <link rel="stylesheet" type="text/css" href="foundation-6.3.1-complete/foundation.min.css"/>
-    <link rel="stylesheet" type="text/css" href="public/stylesheets/app.cs"/>
+    <link rel="stylesheet" type="text/css" href="public/stylesheets/app.css"/>
   </head>
   
   <body>
@@ -39,27 +39,69 @@
       </nav>
     </header>
       <!-- #Content-->
-    <div>
-      <h2>Welcome!</h2>
-      <p>
-        Info will go here
-      </p>
-      <h2>About Us</h2>
-      <p>
+    <div id="mainDiv">
+      <div class="row">
+        <h2 class="small-8 large-6 columns">Welcome!</h2>
+      </div>
+      <div class="row">
+        <p class="small-8 large-4 columns">
+          Info will go here
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <h2 class="small-6 large-4 columns">About Us</h2>
+    </div>
+    <div class="row">
+      <p class="small-8 large-6 columns">
         Some more will go here
       </p>
-      <h2>Make An Appointment</h2>
-      <p>
-        And so on
+    </div>
+    <div class="row">
+      <h2 class="small-6 large-8 columns">Make An Appointment</h2>
+    </div>
+    <div class="row">
+      <p class="small-8 large-6 columns">
+        Complete this form to request an appointment. We will be in touch regarding availability soon.
       </p>
-      <h2>Contact Us</h2>
-      <p>
+    </div>
+    <form name="AppointmentForm" action=""
+    <div class="row">
+      <h2 class="small-6 large-4 columns">Contact Us</h2>
+    </div>
+    <div class="row">
+      <p class="small-8 large-6 columns">
         etc.
       </p>
     </div>
-    <footer>
+    <div class="scrollPic">
+      <!-- start slipsum code -->
+  
+      <h2>Hold on to your butts</h2>
+      <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+  
+      <h2>I can do that</h2>
+      <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+  
+      <h2>Are you ready for the truth?</h2>
+      <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+  
+      <h2>I can do that</h2>
+      <p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+  
+      <h2>Are you ready for the truth?</h2>
+      <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+  
+      <!-- end slipsum code -->
+    </div>
+    <footer class="row">
       <!-- footer stuff -->
-      Footer stuff ;)
+      <p class="small-5 large-6 columns">
+        Footer stuff ;)
+      </p>
+      <p class="small-7 large-6 columns">
+        Some more footer stuff :')
+      </p>
     </footer>
     <script src="foundation-6.3.1-complete/js/vendor/jquery.js"></script>
     <script src="foundation-6.3.1-complete/js/vendor/what-input.js"></script>
