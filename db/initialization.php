e2689059d8734ead3b8a38f5b4bc375511cb63a3
Justin Piper <jpiper4198@gmail.com>
2017-06-01 17:59:50 -0400

    Fixed up db initialization

--- db/initialization.php
+++ db/initialization.php
@@ -21,19 +21,19 @@ if($connection->connect_error){
 
 $table = "create table appointment(
   id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  first_date DATETIME_INTERVAL_CODE not null,
-  second_date DATETIME_INTERVAL_CODE ,
-  third_date DATETIME_INTERVAL_CODE ,
+  first_date DATETIME not null,
+  second_date DATETIME,
+  third_date DATETIME,
   creationDate TIMESTAMP 
-  )
+  );
   CREATE TABLE patient(
     id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (20) not null,
     last_name VARCHAR (20) not null,
     email VARCHAR (50) not null,
-    phone NUMBER (10),
+    phone INT (10),
     regDate TIMESTAMP 
-  )
+  );
   CREATE  TABLE  user(
     id INT (5) unsigned auto_increment PRIMARY KEY,
     first_name varchar(20) not NULL,
@@ -45,5 +45,5 @@ $table = "create table appointment(
 if (mysqli_query($connection, $table)) {
   echo 'tables created successfully';
 } else {
-  echo 'failed to generate tables . ' mysqli_error($connection);
+  echo 'failed to generate tables ' . mysqli_error($connection);
 }
