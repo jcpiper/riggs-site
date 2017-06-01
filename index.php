a8f988f8635da11e2e42c6c0ddde9935e4b5180e
Justin Piper <jpiper4198@gmail.com>
2017-06-01 18:38:39 -0400

    ipsum + potential styling

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-  include "db/db_creation.php";
-  include "db/initialization.php";
+//  include "db/db_creation.php";
+//  include "db/initialization.php";
 ?>
 <!doctype html>
 <html>
@@ -48,21 +48,37 @@
         <h2 class="small-8 large-6 columns">Welcome!</h2>
       </div>
       <div class="row">
-        <p class="small-8 large-4 columns">
-          Info will go here
-        </p>
+        <div class="small-12 columns">
+          <!-- start slipsum code -->
+  
+          <p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+  
+          <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+  
+          <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+  
+          <!-- end slipsum code -->
+        </div>
       </div>
     </div>
     <div class="row">
       <h2 class="small-6 large-4 columns">About Us</h2>
     </div>
     <div class="row">
-      <p class="small-8 large-6 columns">
-        Some more will go here
-      </p>
+      <div class="small-8 large-6 columns">
+        <!-- start slipsum code -->
+  
+        <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+  
+        <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+  
+        <p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+  
+        <!-- end slipsum code -->
+      </div>
     </div>
     <div class="row">
-      <h2 class="small-6 large-8 columns">Make An Appointment</h2>
+      <h2 class="small-6 large-centered columns">Make An Appointment</h2>
     </div>
     <?php
     // **************************************************
@@ -70,7 +86,7 @@
     // **************************************************
     ?>
     <div class="row">
-      <p class="small-12 large-6 columns">
+      <p class="small-6 large-centered columns">
         Complete this form to request an appointment. We will be in touch regarding availability soon.
       </p>
     </div>
@@ -156,24 +172,28 @@
       </p>
     </div>
     <div class="scrollPic">
-      <!-- start slipsum code -->
+      <div class="row">
+        <div class="small-10">
+          <!-- start slipsum code -->
   
-      <h2>Hold on to your butts</h2>
-      <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+          <h2>Hold on to your butts</h2>
+          <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
   
-      <h2>I can do that</h2>
-      <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+          <h2>I can do that</h2>
+          <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
   
-      <h2>Are you ready for the truth?</h2>
-      <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+          <h2>Are you ready for the truth?</h2>
+          <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
   
-      <h2>I can do that</h2>
-      <p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+          <h2>I can do that</h2>
+          <p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
   
-      <h2>Are you ready for the truth?</h2>
-      <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+          <h2>Are you ready for the truth?</h2>
+          <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
   
-      <!-- end slipsum code -->
+          <!-- end slipsum code -->
+        </div>
+      </div>
     </div>
     <footer class="row">
       <!-- footer stuff -->
